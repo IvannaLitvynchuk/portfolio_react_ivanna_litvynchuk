ebcf481528015db14c96ea3f708bf4c05171975e
Іванна Літвинчук <Yana@MacBook-Air-Ivanna.local>
2023-01-20 19:18:32 +0200

    updated modal in portfolio

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { projects } from "../data/projects";
 
-
-const Modal = () => {
+const Modal = (props) => {
+    
+    const projectId = props.projectId;
+    // console.log(projectId)
+    const item = projects.find(element => element.id === projectId)
+    // console.log(item)
     return (
         <>
-            <div className="window" onClick={onClose}></div>
-            <div className="modal-window">
-                    my modal
-                    <button className="close-window" onClick={onClose}>Close</button>
+            <div>
+                <h3 className="window-title">{item.title}</h3>
+                <p className="window-text">You can see the project code at the following link: <a href={item.link_into_github}>{item.title.toLowerCase()}</a>.</p>
+                <p className="window-text">You can see the project site at the following link: <a href={item.link_into_site}>{item.title.toLowerCase()}</a>.</p>
+                <p className="window-text">{item.description}</p>
             </div>
         </>
     )
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,13 +1,22 @@
 import React, {useState} from "react";
 import { projects } from "../data/projects";
 import { TailSpin } from 'react-loader-spinner';
+import Modal from "../components/Modal";
 
 const Portfolio = () => {
+const [modal, setModal] = useState(false)
 
+const [projectId, setProjectId] = useState()
 const [loading, setLoading] = useState(true)
-console.log(projects)
-const onOpenModal = () => {
-    console.log("work")
+
+const onOpenModal = (e) => {
+    e.stopPropagation()
+    setModal(true)
+    setProjectId(Number(e.currentTarget.getAttribute("id")))
+}
+
+const onCloseModal = () => {
+    setModal(false)
 }
     return (
         <section id="portfolio" className="portfolio">
@@ -15,13 +24,23 @@ const onOpenModal = () => {
                 <h2>— Portfolio —</h2>
             </div>
             <div>
+            {modal ?
+                <>
+                    <div className="window" onClick={onCloseModal}></div>
+                    <div className="modal-window">
+                        <Modal projectId={projectId} />
+                        <button className="close-window" onClick={onCloseModal}>Close</button>
+                    </div>
+                </>:
+            ""
+            }
             { projects.length > 0 ?
                 <div className='project-card-bg' >
                     {projects.map(project => (
-                        <div className="container-projects">
-                            <div className="project-card" key={project.id}>
+                        <div className="container-projects" key={project.id}>
+                            <div className="project-card" >
                                 <img className="project-card-img" src={project.image} style={{width: "300px", height: "250px"}} />
-                                <button
+                                <button id={project.id}
                                 className="open-window"
                                 onClick={onOpenModal} >
                                     <b>Details</b>
